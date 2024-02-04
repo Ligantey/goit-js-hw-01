04ec4439600acb447299aa642bf6843ca3acd522
Galyna Rogovska <115642878+Ligantey@users.noreply.github.com>
2024-02-04 17:42:41 +0100

    upgrade_task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,13 @@
 // Задача 3. Ширина елемента
 
 function getElementWidth(content, padding, border) {
-   
-    contentWidth = Number.parseFloat(content);
-    paddingWidth = Number.parseFloat(padding);
-    borderWidth = Number.parseFloat(border);
+  contentWidth = Number.parseFloat(content);
+  paddingWidth = Number.parseFloat(padding);
+  borderWidth = Number.parseFloat(border);
 
-    return (contentWidth + paddingWidth * 2 + borderWidth * 2 );
+  return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
